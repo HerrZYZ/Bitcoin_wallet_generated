69f15231a204d3b91fc093d6e6315134ed7e18a7
HerrZYZ <45750637+HerrZYZ@users.noreply.github.com>
2023-08-29 21:17:07 +1000

    Update user.py

--- user.py
+++ user.py
@@ -1,152 +1,23 @@
-## Required Python third-party packages
+## Code: user.py
 ```python
-"""
-flask==1.1.2
-flask_socketio==5.0.1
-bcrypt==3.2.0
-bitcoinlib==0.4.14
-sqlite3==2.6.0
-"""
-```
+import bcrypt
+from wallet import Wallet
 
-## Required Other language third-party packages
-```python
-"""
-bootstrap==4.6.0
-jquery==3.5.1
-"""
-```
+class User:
+    def __init__(self, username: str, password: str, email: str):
+        self.username = username
+        self.password = self.hash_password(password)
+        self.email = email
+        self.wallet = None
 
-## Full API spec
-```python
-"""
-openapi: 3.0.0
-info:
-  version: 1.0.0
-  title: Bitcoin Wallet API
-paths:
-  /user:
-    post:
-      summary: Create a new user
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              $ref: '#/components/schemas/User'
-      responses:
-        '200':
-          description: User created successfully
-  /wallet:
-    post:
-      summary: Create a new wallet for a user
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              $ref: '#/components/schemas/Wallet'
-      responses:
-        '200':
-          description: Wallet created successfully
-  /transaction:
-    post:
-      summary: Create a new transaction
-      requestBody:
-        required: true
-        content:
-          application/json:
-            schema:
-              $ref: '#/components/schemas/Transaction'
-      responses:
-        '200':
-          description: Transaction created successfully
-components:
-  schemas:
-    User:
-      type: object
-      properties:
-        username:
-          type: string
-        password:
-          type: string
-        email:
-          type: string
-    Wallet:
-      type: object
-      properties:
-        address:
-          type: string
-        balance:
-          type: number
-    Transaction:
-      type: object
-      properties:
-        sender:
-          type: string
-        recipient:
-          type: string
-        amount:
-          type: number
-"""
-```
+    def hash_password(self, password: str) -> bytes:
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-## Logic Analysis
-```python
-[
-    ("main.py", "Contains the main entry of the application, initializes Flask app and routes"),
-    ("user.py", "Contains User class and its methods, should be implemented first as it is a prerequisite for Wallet and Transaction"),
-    ("wallet.py", "Contains Wallet class and its methods, depends on User class"),
-    ("transaction.py", "Contains Transaction class and its methods, depends on Wallet class"),
-    ("notification.py", "Contains Notification class and its methods, depends on User class"),
-    ("templates/*.html", "Contains the frontend of the application, depends on all backend classes"),
-    ("static/css/main.css", "Contains the styles for the frontend, can be developed in parallel with HTML templates"),
-    ("static/js/main.js", "Contains the JavaScript for the frontend, depends on all backend classes and HTML templates")
-]
-```
+    def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
 
-## Task list
-```python
-[
-    "user.py",
-    "wallet.py",
-    "transaction.py",
-    "notification.py",
-    "main.py",
-    "templates/index.html",
-    "templates/login.html",
-    "templates/register.html",
-    "templates/transaction.html",
-    "templates/notification.html",
-    "static/css/main.css",
-    "static/js/main.js"
-]
+    def create_wallet(self) -> Wallet:
+        if self.wallet is None:
+            self.wallet = Wallet()
+        return self.wallet
 ```
-
-## Shared Knowledge
-```python
-"""
-'bitcoinlib' is a Python library for Bitcoin and other cryptocurrencies. It provides functionalities for creating and managing wallets, making transactions, etc.
-'flask' is a web framework for Python. It is used to create the backend of the application.
-'flask_socketio' is a Flask extension for handling WebSocket connections. It is used for real-time balance change notifications.
-'bcrypt' is a Python library for hashing passwords. It is used to secure user passwords.
-'bootstrap' and 'jquery' are frontend libraries. They are used to create a responsive and user-friendly interface.
-"""
-```
-
-## Anything UNCLEAR
-The requirement is clear. We need to start with the backend classes (User, Wallet, Transaction, Notification) as they are the prerequisites for the frontend. The frontend (HTML templates, CSS, JavaScript) can be developed in parallel once the backend is ready.
-[
-    "user.py",
-    "wallet.py",
-    "transaction.py",
-    "notification.py",
-    "main.py",
-    "templates/index.html",
-    "templates/login.html",
-    "templates/register.html",
-    "templates/transaction.html",
-    "templates/notification.html",
-    "static/css/main.css",
-    "static/js/main.js"
-]
